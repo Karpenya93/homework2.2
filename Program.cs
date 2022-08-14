ecf7fb9ebbc51953f7b0198344acf791722a3e54
Karpenya <karpenyalexa1993@gmail.com>
2022-08-14 07:56:21 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Dictionary<string,string> translate=new Dictionary<string,string>()
+{
+    { "green","Зеленый"},
+    { "yellow", "Желтый"},
+    { "blue", "Синий"},
+    { "garden", "Сад"},
+    { "milk", "Молоко"},
+    { "mouse", "Мышь"},
+    { "vodka", "Водка"},
+    { "dog", "Пес"},
+    { "cat", "Кошка"},
+    { "Game", "Игра"},
+    { "car", "Машина"},
+
+};
+
+while (true)
+{
+    string word = Console.ReadLine().ToLower();
+    if (translate.ContainsKey(word))
+    {
+        Console.WriteLine(translate[word]);
+    }
+    else Console.WriteLine("Такого значения нету ");
+}
+
+
